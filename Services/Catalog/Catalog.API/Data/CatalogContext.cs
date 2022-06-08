538b52c712770a2dacb0adf92f93ca6a7a7fd933
Ali Chavoshi <alichavoshii1372@gmail.com>
2022-06-08 08:32:42 +0430

    11. Developing Data Layer - Connect Mongo Docker Container from Catalog.API

---
+++ Services/Catalog/Catalog.API/Data/CatalogContext.cs
@@ -0,0 +1,17 @@
+﻿using Catalog.API.Entities;
+using MongoDB.Driver;
+
+namespace Catalog.API.Data;
+
+public class CatalogContext : ICatalogContext
+{
+    public CatalogContext(IConfiguration configuration)
+    {
+        var client = new MongoClient(configuration["DatabaseSettings:ConnectionString"]);
+        var database = client.GetDatabase(configuration["DatabaseSettings:DatabaseName"]);
+        Products = database.GetCollection<Product>(configuration["DatabaseSettings:CollectionName"]);
+        CatalogContextSeed.SeedData(Products);
+    }
+
+    public IMongoCollection<Product> Products { get; }
+}
