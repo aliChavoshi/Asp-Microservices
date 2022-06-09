8667274f2567ddd50a5761bfa428ff38cde0de1d
Ali Chavoshi <alichavoshii1372@gmail.com>
2022-06-09 08:47:10 +0430

    add shopping cart entity for basket

---
+++ Services/Basket/Basket.API/Entities/ShoppingCart.cs
@@ -0,0 +1,21 @@
+﻿namespace Basket.API.Entities;
+
+public class ShoppingCart
+{
+    public ShoppingCart()
+    {
+    }
+
+    public ShoppingCart(string userName)
+    {
+        UserName = userName;
+    }
+
+    public string UserName { get; set; }
+    public List<ShoppingCartItem> Items { get; set; } = new();
+
+    public decimal TotalPrice
+    {
+        get { return Items.Sum(x => x.Price * x.Quantity); }
+    }
+}
